3c0153b95dad615580a8ae13faeb2268843d3249
pepelev <pepelev@skbkontur.ru>
2020-12-25 00:19:43 +0500

    removes rudimentary code

--- src/FunBot/Aspect.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FunBot
-{
-    public abstract class Aspect<T>
-    {
-        public abstract bool Changed(T old, T @new);
-        public abstract string Print(T old, T @new);
-    }
-}
